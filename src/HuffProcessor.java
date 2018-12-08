bf2e0c358d3a9440b00d52bbedc611bf1af52beb
jackwood11 <jack.wood@duke.edu>
2018-12-08 14:31:27 -0500

    a

--- src/HuffProcessor.java
+++ src/HuffProcessor.java
@@ -82,7 +82,7 @@ public class HuffProcessor {
 		//if(val == -1) 
 		//	throw new HuffException("Not a valid input.");
 		if(root.myLeft != null || root.myRight != null) {
-			out.write(0);
+			out.writeBits(1,0);
 			writeHeader(root.myLeft, out);
 			writeHeader(root.myRight, out);
 			
